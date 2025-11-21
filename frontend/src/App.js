48699a386a6d28321b128ccf59b5cbfb36f804cc
Kumud <kumud0629@gmail.com>
2025-11-21 23:09:13 +0530

    login,signup added(edited)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Login from "./login";
 import Signup from "./signup";
 
 function App() {
-  return <Signup />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+
 
 
 
